a3173ce44423a7c25cd390f05d63328e82787dc9
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2023-07-19 19:52:45 +0530

    Add sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {Stack} from '@mui/material';
 import { Link } from 'react-router-dom';
 import {logo} from '../utils/constants';
+import SearchBar from './SearchBar';
 
 const Navbar = () =>(
    <Stack direction='row' alignItems='center' p={2} 
@@ -8,6 +9,7 @@ const Navbar = () =>(
    <Link to='/' style={{display:'flex', alignItems:'center'}}>
     <img src={logo} alt='logo' height={45}/>
    </Link>
+   <SearchBar/>
    </Stack>
   )
 
