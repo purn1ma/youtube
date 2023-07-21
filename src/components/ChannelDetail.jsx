9b5a9d812bb4cf89006578953e6ce5dcf95832dc
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2023-07-21 16:34:44 +0530

    add channelcard

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import {Videos,ChannelCard} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
+    const [channelDetail, setChannelDetail] = useState(null);
+    const {id} = useParams();
+      console.log(channelDetail)
+    useEffect(() => {
+       fetchFromAPI(`channel?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+    }, [id])
+
+  
 }
 
 export default ChannelDetail
